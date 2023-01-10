0c270297ae3712e6c475a7acfc59d291fb984b18
osamawahab1000 <02-134182-099@student.bahria.edu.pk>
2023-01-11 04:08:42 +0500

    working on menu page and add js file

--- POS_System/views.py
+++ POS_System/views.py
@@ -5,5 +5,7 @@ from .models import Product
 # Create your views here.
 def index(request):
     products = Product.objects.all()
+
     dicto = {'products': products}
+
     return render(request, 'POS_System/index.html', dicto)
